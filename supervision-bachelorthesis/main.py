acfa69fa9b08756f4bd9be02013a1df3a4e37523
pschenk <pascal.schenk@aponia.de>
2024-04-30 11:04:29 +0200

    feat: add polygon zone for perspective transformation

--- supervision-bachelorthesis/main.py
+++ supervision-bachelorthesis/main.py
@@ -1,14 +1,47 @@
 import argparse
 
 import cv2
+import numpy as np
 from ultralytics import YOLO
 
 import supervision as sv
 
+# THIS SHOULD BE CHANGED FOR EVERY VIDEO
+# TODO: Polygon is not drawn correctly, coordinates should be accurate for highway_test.mp4 (check: https://roboflow.github.io/polygonzone/)
+SOURCE = np.array([[1007, 563], [2317, 823], [937, 1023], [467, 583]])
+# THIS SHOULD BE CHANGED FOR EVERY VIDEO
+TARGET_WIDTH = 25
+# THIS SHOULD BE CHANGED FOR EVERY VIDEO
+TARGET_HEIGHT = 150
+
+TARGET = np.array(
+    [
+        [0, 0],
+        [TARGET_WIDTH - 1, 0],
+        [TARGET_WIDTH - 1, TARGET_HEIGHT - 1],
+        [0, TARGET_HEIGHT - 1]
+    ]
+)
+
+
+class ViewTransformer:
+    def __init__(self, source: np.ndarray, target: np.ndarray):
+        source = source.astype(np.float32)
+        target = target.astype(np.float32)
+        self.m = cv2.getPerspectiveTransform(source, target)
+
+    def transform_points(self, points: np.ndarray) -> np.ndarray:
+        if points.size == 0:
+            return points
+
+        reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
+        transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
+        return transformed_points.reshape(-1, 2)
+
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description='Bachelorarbeit Supervision'
+        description='Bachelorthesis Supervision'
     )
     parser.add_argument(
         "--source_video_path",
@@ -34,18 +67,29 @@ if __name__ == "__main__":
 
     frame_generator = sv.get_video_frames_generator(args.source_video_path)
 
+    polygon_zone = sv.PolygonZone(SOURCE)
+    view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
+
     for frame in frame_generator:
         result = model(frame)[0]
         detections = sv.Detections.from_ultralytics(result)
+        detections = detections[polygon_zone.trigger(detections)]
         detections = byte_track.update_with_detections(detections=detections)
 
+        points = detections.get_anchors_coordinates(anchor=sv.Position.BOTTOM_CENTER)
+        points = view_transformer.transform_points(points=points).astype(int)
+
         labels = [
-            f"#{tracker_id}"
-            for tracker_id
-            in detections.tracker_id
+            f"x: {x}, y: {y}"
+            for [x, y] in points
+
+            # f"#{tracker_id}"
+            # for tracker_id
+            # in detections.tracker_id
         ]
 
         annotated_frame = frame.copy()
+        annotated_frame = sv.draw_polygon(annotated_frame, polygon=SOURCE, color=sv.Color.RED)
         annotated_frame = bounding_box_annotator.annotate(scene=annotated_frame, detections=detections)
         annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
 
