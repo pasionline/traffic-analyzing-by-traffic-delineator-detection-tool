2cd26dc64f182da3d96e5d948eb7ba108aa48c99
pschenk <pascal.schenk@aponia.de>
2025-02-17 22:11:30 +0100

    feat: enhance inference precision with supervision

--- supervision-bachelorthesis/get-coordinates-of-delineators.py
+++ supervision-bachelorthesis/get-coordinates-of-delineators.py
@@ -1,22 +1,35 @@
 import cv2
-from ultralytics import YOLOv10
+from ultralytics import YOLO
 import supervision as sv
+import numpy as np
 
-model = YOLOv10(f'bestv6.pt')
-frame_generator = sv.get_video_frames_generator('crash_test.mp4')
+model = YOLO(f'best_250210.pt')
+frame_generator = sv.get_video_frames_generator('typicalGermanAutobahn.mkv')
+
 image = next(frame_generator)
 
-results = model(source=image, conf=0.25)
-print(results[0].boxes.xyxy)
+def callback(image_slice: np.ndarray) -> sv.Detections:
+    result = model(image_slice)[0]
+    return sv.Detections.from_ultralytics(result)
 
 
-detections = sv.Detections.from_ultralytics(results[0])
+slicer = sv.InferenceSlicer(callback=callback)
+detections = slicer(image)
 
-bounding_box_annotator = sv.BoundingBoxAnnotator()
+i = 0
+while i < 50:
+    image = next(frame_generator)
+    nextDetections = slicer(image)
+    if len(nextDetections.xyxy) > len(detections.xyxy):
+        detections = nextDetections
+    i += 1
+
+print(detections.xyxy)
+
+box_annotator = sv.BoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 
-
-annotated_image = bounding_box_annotator.annotate(
+annotated_image = box_annotator.annotate(
     scene=image, detections=detections)
 annotated_image = label_annotator.annotate(
     scene=annotated_image, detections=detections)
